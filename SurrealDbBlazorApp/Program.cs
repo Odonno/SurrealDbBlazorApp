9ee83be0e760b4f6532098d5137f878867a1e27f
Odonno <bottiau.david@laposte.net>
2024-06-25 19:15:20 +0200

    🎉 create example project

---
+++ SurrealDbBlazorApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using SurrealDb.Net;
+using SurrealDbBlazorApp;
+using SurrealDbBlazorApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+string connectionString = builder.Configuration.GetConnectionString("SurrealDB") ?? throw new ArgumentNullException();
+
+// 💡 Consider using Singleton unless you need per-session instance (like when using auth features)
+builder.Services.AddSurreal(connectionString, ServiceLifetime.Singleton);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(SurrealDbBlazorApp.Client._Imports).Assembly);
+
+await InitializeDbAsync();
+
+app.Run();
+
+async Task InitializeDbAsync()
+{
+    var surrealDbClient = new SurrealDbClient(
+        SurrealDbOptions.Create()
+            .FromConnectionString(builder.Configuration.GetConnectionString("SurrealDB")!)
+            .Build()
+    );
+
+    var tasks = new[]
+    {
+        GenerateWeatherForecastsAsync(surrealDbClient),
+    };
+
+    await Task.WhenAll(tasks);
+}
+
+async Task GenerateWeatherForecastsAsync(ISurrealDbClient surrealDbClient)
+{
+    const int initialCount = 5;
+    var weatherForecasts = new WeatherForecastFaker().Generate(initialCount);
+
+    var tasks = weatherForecasts.Select(weatherForecast =>
+        surrealDbClient.Create(WeatherForecast.Table, weatherForecast)
+    );
+
+    await Task.WhenAll(tasks);
+}
---
+++ SurrealDbBlazorApp/WeatherForecast.cs
@@ -0,0 +1,12 @@
+﻿namespace SurrealDbBlazorApp;
+
+public class WeatherForecast
+{
+    internal const string Table = "weatherForecast";
+
+    public DateTime Date { get; set; }
+    public string? Country { get; set; }
+    public int TemperatureC { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public string? Summary { get; set; }
+}
